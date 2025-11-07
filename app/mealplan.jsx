8ed0adf7d9be5efd78a676200ce6bd38e447b861
Kyle Lund <lundkyl@oregonstate.edu>
2025-11-07 14:59:20 -0800

    Added meal planning route

---
+++ app/mealplan.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function GroceryList() {
+    return(
+        <View>
+            <Text>meal plan page</Text>
+        </View>
+    );
+}

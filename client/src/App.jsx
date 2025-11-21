e6f9b63314090849484f46c8ff5ea24ef26c33ca
Xander <sniffenx@oregonstate.edu>
2025-11-20 17:06:55 -0800

    Added Basic Text to Meal Planner Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import MealPlanner from "./pages/MealPlanner";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       {/* Pages */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/mealplanner" element={<MealPlanner />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/MealPlanner.jsx
@@ -0,0 +1,13 @@
+
+export default function MealPlanner() {
+    return (
+        <div className="px-8 py-12">
+            <section className="text-center">
+                <h1 className="text-4xl font-bold mb-4">Meal Planner Coming Soon!</h1>
+                <p className="text-gray-600 max-w-xl mx-auto">
+                    We're working hard to bring you an amazing meal planning experience. Stay tuned!
+                </p>
+            </section>
+        </div>
+    );
+}

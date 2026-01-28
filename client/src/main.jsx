42128c04163a56f55574286072edcc938066607e
Kyle Lund <lundkyl@oregonstate.edu>
2026-01-27 21:44:52 -0800

    Connect front end to backend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Amplify } from 'aws-amplify';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import outputs from '../../amplify_outputs.json';
+import App from './App.jsx';
+import './index.css';
+
+Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

4a1e7d3420d4d5a754dfc4ea57640dc276452c01
Kyle Lund <lundkyl@oregonstate.edu>
2026-01-28 19:38:28 -0800

    feat: aws amplify setup kl (#51)
    * feat:deployed app with AWS Amplify
    
    * Trigger deployment
    
    * Connect front end to backend
    
    * Add AWs-amplify dependency in client json
    
    * Moved amplify json in client folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { Amplify } from 'aws-amplify';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import outputs from '../../amplify_outputs.json';
+import outputs from '../amplify_outputs.json';
 import App from './App.jsx';
 import './index.css';
 

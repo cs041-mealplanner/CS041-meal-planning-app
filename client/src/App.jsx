85d89a0cc4b479b7ab28e9cbb4a721b2a9068cdf
Mint Lapatrada <liawpail@oregonstate.edu>
2026-02-13 00:22:17 -0800

    Add dishes json, grocery-list and ingredient page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import MealPlanner from "./pages/MealPlanner";
+
+import GroceryList from './pages/grocery-list';
+import IngredientPage from './pages/ingredient-page';
 
 export default function App() {
   return (
@@ -16,6 +20,10 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/mealplanner" element={<MealPlanner />} />
         <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/recipe/:id" element={<IngredientPage />} />
+        <Route path="/grocery" element={<GroceryList />} />
+
       </Routes>
     </BrowserRouter>
   );

cc9f230aadc724d2b4142f19b8592f1f08d4f97b
Kyle Lund <lundkyl@oregonstate.edu>
2026-02-15 16:01:40 -0800

    added  sign in logic and user schema

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Navbar";
+import ConfirmSignup from "./pages/confirmSignup";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import MealPlanner from "./pages/MealPlanner";
+import SignUp from "./pages/sign-in";
 
 export default function App() {
   return (
@@ -16,6 +18,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/mealplanner" element={<MealPlanner />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/confirm-signup" element={<ConfirmSignup />} />
       </Routes>
     </BrowserRouter>
   );
